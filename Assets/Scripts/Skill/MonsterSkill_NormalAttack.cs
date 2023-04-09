9b587dbdf0709f411458abae9bb4e4a8d64eeec4
tristan0613 <114924101+tristan0613@users.noreply.github.com>
2023-04-10 00:02:05 +0900

    몬스터 추가 및 파일 변경

--- Assets/Scripts/Skill/MonsterSkill_NormalAttack.cs
+++ Assets/Scripts/Skill/MonsterSkill_NormalAttack.cs
@@ -2,14 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Skill_GroundBreak : BaseSkill
+public class MonsterSkill_NormalAttack : BaseSkill
 {
-    public ManualCollision groundBreakAttackCollision; // 나중에 스킬이 늘어나면 배열로 바꿔줘서, ExcuteAttack에 강화정도의 인덱스를 넣어준다. 그 인덱스
+    public ManualCollision monsterAttackCollision;
 
 
     public override void ExcuteAttack(GameObject target = null, Transform startPoint = null)
     {
-        Collider[] colliders = groundBreakAttackCollision?.CheckOverlapBox(targetMask);
+        Collider[] colliders = monsterAttackCollision?.CheckOverlapBox(targetMask);
 
 
         // CheckOverlapBox을 통해 얻어온 충돌체마다 데미지 처리를 해준다. 
@@ -17,18 +17,19 @@ public class Skill_GroundBreak : BaseSkill
         {
             collider.gameObject.GetComponent<IDamageable>()?.TakeDamage((int)(damage), effectPrefab);
         }
+
+
     }
+
 
     public override void ExcuteParticleSystem()
     {
-
-        SkillManager.instance.SpawnParticle(ESkill.GroundBreak, ESkillParticleType.Spawn);
-
+       
     }
 
     public override void ExitParticleSystem()
     {
-
+       
 
     }
 }
